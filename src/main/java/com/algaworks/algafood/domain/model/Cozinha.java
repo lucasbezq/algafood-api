6fd8eaa0e8669e9f974d92b237043d0cfeeb2e76
Lucas Ezequiel <lucasbarrosez@gmail.com>
2024-01-16 20:21:28 -0300

    feat: adicionar entidades de dominio

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+}
---
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private BigDecimal taxaFrete;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Cozinha cozinha;
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+
+}

43821cc8deab47e34b892b2a2d6ce9fb4d46cfe3
Lucas Ezequiel <lucasbarrosez@gmail.com>
2024-02-24 17:17:39 -0300

    feat: adicionar metódos responsaveis por criar e atualizar um recurso de cidades

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,13 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.service.CadastroCidadeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +18,9 @@ public class CidadeController {
 
     @Autowired
     private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private CadastroCidadeService cadastroCidadeService;
 
     @GetMapping
     public List<Cidade> listar() {
@@ -34,4 +38,33 @@ public class CidadeController {
         return ResponseEntity.ok(cidade);
     }
 
+    @PostMapping
+    public ResponseEntity<?> adicionar(@RequestBody Cidade cidade) {
+
+        try {
+            var novaCidade = cadastroCidadeService.salvar(cidade);
+            return ResponseEntity.status(HttpStatus.CREATED).body(novaCidade);
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping(path = "/{cidadeId}")
+    public ResponseEntity<?> atualizar(@PathVariable Long cidadeId, @RequestBody Cidade cidade) {
+        try {
+            var cidadeAtual = cidadeRepository.buscar(cidadeId);
+
+            if (cidadeAtual == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+            BeanUtils.copyProperties(cidade, cidadeAtual, "id");
+            cidadeAtual = cadastroCidadeService.salvar(cidadeAtual);
+            return ResponseEntity.ok().body(cidadeAtual);
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }

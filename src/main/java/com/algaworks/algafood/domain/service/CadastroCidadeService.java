4e693514fed7de435189fc23eca3831ac94162bf
Lucas Ezequiel <lucasbarrosez@gmail.com>
2024-02-24 17:16:27 -0300

    feat: adicionar serviço de cadastro de cidades

---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroCidadeService {
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public Cidade salvar(Cidade cidade) {
+        var estadoId = cidade.getEstado().getId();
+        var estado = estadoRepository.buscar(estadoId);
+
+        if (estado == null) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe cadastro de Estado com o código: %d", estadoId));
+        }
+
+        cidade.setEstado(estado);
+        return cidadeRepository.salvar(cidade);
+    }
+}

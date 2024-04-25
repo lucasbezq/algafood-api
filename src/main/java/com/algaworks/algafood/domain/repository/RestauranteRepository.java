33b463d868f127cfe6fd2cb59af2bee678887c66
lucasbezq <lucasbarrosez@gmail.com>
2024-04-25 08:41:34 -0300

    feat: externalizar consultas JPQL

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,14 +2,24 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
+
+    Optional<Restaurante> findFirstByNomeContaining(String nome);
+
+    boolean existsByNome(String nome);
+
+    int countByCozinhaId(Long id);
 }

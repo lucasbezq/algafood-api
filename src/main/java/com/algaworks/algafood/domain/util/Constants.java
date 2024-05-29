99509e5c4a66b88dbe1dbe9c7622fc7e4a41e93b
lucasbezq <lucasbarrosez@gmail.com>
2024-05-29 20:44:11 -0300

    feat: criar classe utilitária responsável por armazenar constantes

---
+++ src/main/java/com/algaworks/algafood/domain/util/Constants.java
@@ -0,0 +1,10 @@
+package com.algaworks.algafood.domain.util;
+
+public class Constants {
+
+    public static final String MSG_NAO_EXISTE_CADASTRO_DE_ESTADO = "Não existe um cadastro de Estado com o código %d.";
+    public static final String MSG_COZINHA_NAO_ENCONTRADA = "Não existe um cadastro de cozinha com o código %d";
+    public static final String MSG_COZINHA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
+    public static final String MSG_ESTADO_EM_USO = "Estado de código %d não pode ser removida, pois está em uso";
+
+}

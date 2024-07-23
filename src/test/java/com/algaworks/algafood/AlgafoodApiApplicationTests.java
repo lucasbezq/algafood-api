007e418b70b36f99ac4b1dea4d4e800b96140d9c
lucasbezq <lucasbarrosez@gmail.com>
2024-07-22 21:26:58 -0300

    feat: preparar projeto para testes de integração

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.algaworks.algafood;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+public class AlgafoodApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }

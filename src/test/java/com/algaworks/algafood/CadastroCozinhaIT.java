02620b047746a56652cd235a90a39da1994c592e
lucasbezq <lucasbarrosez@gmail.com>
2024-07-30 15:44:16 -0300

    feat: validar o body da resposta HTTP

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -2,12 +2,16 @@ package com.algaworks.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,5 +30,19 @@ public class CadastroCozinhaIT {
                     .get()
                 .then()
                     .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveRetornarConter4Cozinhas_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RestAssured.given()
+                    .basePath("/cozinhas")
+                    .port(port)
+                    .accept(ContentType.JSON)
+                .when()
+                    .get()
+                .then()
+                    .body("", hasSize(4));
     }
 }

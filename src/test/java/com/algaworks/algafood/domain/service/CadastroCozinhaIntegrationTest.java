aace62fc109da1fe2642b47f3460c02710e0e09b
lucasbezq <lucasbarrosez@gmail.com>
2024-07-29 21:46:21 -0300

    feat: implementar testes para cobrir cenários de exceção

--- src/test/java/com/algaworks/algafood/domain/service/CadastroCozinhaIntegrationTest.java
+++ src/test/java/com/algaworks/algafood/domain/service/CadastroCozinhaIntegrationTest.java
@@ -1,7 +1,9 @@
-package com.algaworks.algafood;
+package com.algaworks.algafood.domain.service;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import com.algaworks.algafood.domain.util.Constants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.validation.ConstraintViolationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 
 @RunWith(SpringRunner.class)
@@ -21,7 +24,7 @@ public class CadastroCozinhaIntegrationTest {
     private CadastroCozinhaService service;
 
     @Test
-    public void testarCadastroCozinhaComSucesso() {
+    public void deveAtribuirId_QuandoCadastrasCozinhaComDadosCorrestos() {
         var expectedCozinha = new Cozinha();
         expectedCozinha.setNome("Chinesa");
 
@@ -32,10 +35,24 @@ public class CadastroCozinhaIntegrationTest {
     }
 
     @Test(expected = ConstraintViolationException.class)
-    public void deveFalharAoCadastrarCozinhaSemNome() {
+    public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
         var expectedCozinha = new Cozinha();
 
         service.salvar(expectedCozinha);
+    }
+
+    @Test
+    public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+        try {
+            service.excluir(1L);
+        } catch (EntidadeEmUsoException e) {
+            assertThat(e.getMessage().equals(Constants.MSG_COZINHA_EM_USO));
+        }
+    }
+
+    @Test(expected = CozinhaNaoEncontradaException.class)
+    public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+        service.excluir(10L);
     }
 
 

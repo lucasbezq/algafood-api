93ff9a5b885ca5e9434474999d9df6f0677e9223
lucasbezq <lucasbarrosez@gmail.com>
2024-08-25 00:54:14 -0300

    feat: adicionar a lib modelMapper e utilizar para conversão de objetos

--- src/main/java/com/algaworks/algafood/api/converter/RestauranteConverter.java
+++ src/main/java/com/algaworks/algafood/api/converter/RestauranteConverter.java
@@ -1,23 +1,19 @@
 package com.algaworks.algafood.api.converter;
 
 import com.algaworks.algafood.api.dto.request.RestauranteRequest;
-import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestauranteConverter {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurante toDomain(RestauranteRequest restauranteRequest) {
-        var restaurante = new Restaurante();
-        restaurante.setNome(restauranteRequest.getNome());
-        restaurante.setTaxaFrete(restauranteRequest.getTaxaFrete());
-
-        var cozinha = new Cozinha();
-        cozinha.setId(restauranteRequest.getCozinha().getId());
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return modelMapper.map(restauranteRequest, Restaurante.class);
     }
 
 }
--- src/main/java/com/algaworks/algafood/api/converter/RestauranteDTOConverter.java
+++ src/main/java/com/algaworks/algafood/api/converter/RestauranteDTOConverter.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.api.converter;
 import com.algaworks.algafood.api.dto.CozinhaDTO;
 import com.algaworks.algafood.api.dto.RestauranteDTO;
 import com.algaworks.algafood.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,22 +13,16 @@ import java.util.stream.Collectors;
 @Component
 public class RestauranteDTOConverter {
 
-    public static RestauranteDTO toDTO(Restaurante restaurante) {
-        var cozinhaDTO = new CozinhaDTO();
-        cozinhaDTO.setId(restaurante.getCozinha().getId());
-        cozinhaDTO.setNome(restaurante.getCozinha().getNome());
+    @Autowired
+    private ModelMapper modelMapper;
 
-        var restauranteDTO = new RestauranteDTO();
-        restauranteDTO.setId(restaurante.getId());
-        restauranteDTO.setNome(restaurante.getNome());
-        restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
-        restauranteDTO.setCozinha(cozinhaDTO);
-        return restauranteDTO;
+    public RestauranteDTO toDTO(Restaurante restaurante) {
+        return modelMapper.map(restaurante, RestauranteDTO.class);
     }
 
     public List<RestauranteDTO> toCollectionDTO(List<Restaurante> restaurantes) {
         return restaurantes.stream()
-                .map(RestauranteDTOConverter::toDTO)
+                .map(restaurante -> toDTO(restaurante))
                 .collect(Collectors.toList());
     }
 

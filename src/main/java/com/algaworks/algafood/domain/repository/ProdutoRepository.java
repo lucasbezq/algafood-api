f87771143672fa10dab06e7e97befe99f1c5e53a
lucasbezq <lucasbarrosez@gmail.com>
2024-11-04 19:06:33 -0300

    feat: implementar pesquisas simples na API

--- src/main/java/com/algaworks/algafood/domain/repository/ProdutoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/ProdutoRepository.java
@@ -1,9 +1,11 @@
 package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,6 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     Optional<Produto> findByIdAndRestauranteId(Long produtoId, Long restauranteId);
 
+    List<Produto> findByRestaurante(Restaurante restaurante);
+    List<Produto> findAtivosByRestaurante(Restaurante restaurante);
 }

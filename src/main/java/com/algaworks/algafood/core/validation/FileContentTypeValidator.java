586433897a4d7c29be9275ee00824bbedab9d14e
Lucas Ezequiel <lucasbarrosez@gmail.com>
2025-02-01 20:37:20 -0300

    feat: criar anotation para validar o tipo de conteudo do arquivo

---
+++ src/main/java/com/algaworks/algafood/core/validation/FileContentTypeValidator.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.core.validation;
+
+import org.springframework.http.MediaType;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
+
+    private List<String> allowedTypes;
+
+    @Override
+    public void initialize(FileContentType constraintAnnotation) {
+        this.allowedTypes = Arrays.asList(constraintAnnotation.allowed());
+    }
+
+    @Override
+    public boolean isValid(MultipartFile value, ConstraintValidatorContext constraintValidatorContext) {
+        return value == null || this.allowedTypes.contains(value.getContentType());
+    }
+}

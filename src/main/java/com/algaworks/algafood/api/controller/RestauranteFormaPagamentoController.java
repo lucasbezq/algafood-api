b8aca9969df111e9c6dc722602381d6a11eacdcc
Lucas Ezequiel <lucasbarrosez@gmail.com>
2025-09-10 21:24:25 -0300

    feat: adicionar links de associar e desassociar formas de pagamento no recurso de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -7,7 +7,11 @@ import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/restaurantes/{restauranteId}/formas-pagamento")
@@ -22,19 +26,34 @@ public class RestauranteFormaPagamentoController implements RestauranteFormaPaga
     @GetMapping
     public CollectionModel<FormaPagamentoDTO> listar(@PathVariable Long restauranteId) {
         var restaurante = cadastroRestauranteService.buscarRestaurante(restauranteId);
-        return formaPagamentoDTOConverter.toCollectionModel(restaurante.getFormasPagamento(), restauranteId);
+        var formasPagamentoDTO = formaPagamentoDTOConverter.toCollectionModel(restaurante.getFormasPagamento(), restauranteId);
+
+        formasPagamentoDTO.getContent()
+                .forEach(formaPagamentoDTO -> {
+                    formaPagamentoDTO.add(linkTo(methodOn(RestauranteFormaPagamentoController.class)
+                            .desassociar(restauranteId, formaPagamentoDTO.getId()))
+                            .withRel("desassociar"));
+
+                    formaPagamentoDTO.add(linkTo(methodOn(RestauranteFormaPagamentoController.class)
+                            .associar(restauranteId, formaPagamentoDTO.getId()))
+                            .withRel("associar"));
+                });
+
+        return formasPagamentoDTO;
     }
 
     @DeleteMapping("/{formaPagamentoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void desassociar(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
+    public ResponseEntity<Void> desassociar(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
         cadastroRestauranteService.desassociarFormaPagamento(restauranteId, formaPagamentoId);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{formaPagamentoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void associar(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
+    public ResponseEntity<Void> associar(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
         cadastroRestauranteService.associarFormaPagamento(restauranteId, formaPagamentoId);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
@@ -3,10 +3,7 @@ package com.algaworks.algafood.api.openapi.controller;
 import com.algaworks.algafood.api.dto.FormaPagamentoDTO;
 import io.swagger.annotations.*;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.http.ResponseEntity;
 
 @Api(tags = "Restaurantes")
 public interface RestauranteFormaPagamentoControllerOpenApi {
@@ -19,15 +16,15 @@ public interface RestauranteFormaPagamentoControllerOpenApi {
             @ApiResponse(code = 204, message = "Forma de pagamento desassociada com sucesso."),
             @ApiResponse(code = 404, message = "Restaurante ou forma de pagamento não encontrada.")
     })
-    public void desassociar(@ApiParam(value = "ID de um restaurante", example = "1") Long restauranteId,
-                            @ApiParam(value = "ID de uma forma de pagamento", example = "1")Long formaPagamentoId);
+    public ResponseEntity<Void> desassociar(@ApiParam(value = "ID de um restaurante", example = "1") Long restauranteId,
+                                      @ApiParam(value = "ID de uma forma de pagamento", example = "1")Long formaPagamentoId);
 
     @ApiOperation("Associa uma forma de pagamento a um restaurante")
     @ApiResponses({
             @ApiResponse(code = 204, message = "Forma de pagamento associada com sucesso."),
             @ApiResponse(code = 404, message = "Restaurante ou forma de pagamento não encontrada.")
     })
-    public void associar(@ApiParam(value = "ID de um restaurante", example = "1") Long restauranteId,
+    public ResponseEntity<Void> associar(@ApiParam(value = "ID de um restaurante", example = "1") Long restauranteId,
                          @ApiParam(value = "ID de uma forma de pagamento", example = "1")Long formaPagamentoId);
 
 }
